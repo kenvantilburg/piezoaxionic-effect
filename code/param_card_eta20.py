c5844a334f2eab032318f4b08fd6c9e70f01932d
Amalia <amadden@pitp.ca>
2022-12-07 23:06:13 -0500

    new version
    test commit

--- code/param_card_eta20.py
+++ code/param_card_eta20.py
@@ -54,13 +54,12 @@ k2 = fn_k2_TE(h_11,c_11,beta_11); #EM coupling factor
 ##### READOUT PARAMETERS #####
 ##############################
 
-L_squid = 5e-9 * Henry # SQUID dynamical inductance
-R_squid = 8 * Ohm # SQUID dynamical resistance
+L_squid = 5e-11 * Henry # SQUID dynamical inductance
+R_squid = 300 * Ohm # SQUID dynamical resistance
 
 k_i = 0.75 # SQUID input coupling factor
-L_i = L_squid # input inductance
+L_i = 5e-11 * Henry # input inductance
 k_f = 0.75 # transformer coupling factor
-L_2 = L_i # transformer inductance
 
 eps_L_1 = 1e-6; # loss angle of readout inductor
 
