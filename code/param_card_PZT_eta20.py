ba54dc362f317ec16aa12973b6549f244efb8574
amaliamadden <97406656+amaliamadden@users.noreply.github.com>
2023-10-02 10:07:52 -0400

    Add files via upload

--- code/param_card_PZT_eta20.py
+++ code/param_card_PZT_eta20.py
@@ -10,33 +10,33 @@ Z_S = 88; # charge of Schiff atom
 A_S = 207; # atomic number of Schiff atom
 M_S = Z_S**2 * R_S / BohrRadius**4 # atomic matrix element
 
-rho = 7600 * Kg * Meter**-3 # mass density
+rho = 7500 * Kg * Meter**-3 # mass density
 N_c = 4 # number of atoms in unit cell
 N_S = 1 # number of Schiff atoms
-V_c = amu * A_S * N_S / rho # volume of unit cell
+V_c = 150*10**-30 * Meter**3 # volume of unit cell
 
 n_N = N_S/V_c # number density of Schiff spins
-S_schiff_over_theta = 0.2 * ElectronCharge * fm**3; # Schiff moment proportionality constant with theta
+S_schiff_over_theta = 0.16 * ElectronCharge * fm**3; # Schiff moment proportionality constant with theta
 S_schiff = ThetaAxion * S_schiff_over_theta # Schiff moment from QCD axion DM
 
 c_LA = 1e-3 #mechanical loss angle
-c_11 = 0.5*(1+1j*c_LA) * (N_c * AlphaEM) / (BohrRadius * V_c) / 5 # mechanical stiffness constant
+c_11 = 1e11 * Newton / Meter**2 * (1+1j*c_LA) # mechanical stiffness constant
 
 v = np.sqrt(c_11 / rho) # longitudinal sound speed
 
 eps_LA = 1e-3 #electrical loss angle
-eps_11 = (1-1j*eps_LA) * 1000 # dielectric constant
+eps_11 = (1-1j*eps_LA) * 235 # dielectric constant
 beta_11 = eps_11**-1 # impermittivity constant
 
-zeta_11_over_S = (1/1000) * 4 * np.pi * ElectronCharge * N_S / V_c * (ElectronCharge * BohrRadius**2 / AlphaEM ) * M_S
+zeta_11_over_S = (1/235) * 4 * np.pi * ElectronCharge * N_S / V_c * (ElectronCharge * BohrRadius**2 / AlphaEM ) * M_S
 zeta_11 = zeta_11_over_S * S_schiff # electroaxionic tensor for nuclear coupling
 zeta_11_G_aee = np.sqrt(RhoDMG/2) * (AlphaEM * N_c / V_c) * (ElectronCharge * BohrRadius**2 / AlphaEM) # electroaxionic tensor for electron coupling
 
 
 h_LA = 0e-6
-d_11 = (4 * np.pi * BohrRadius**2 / ElectronCharge) * 200
-h_11 = (1-1j*h_LA) * np.real(beta_11 * d_11 * c_11)
-h_tilde = h_11 / (N_c * ElectronCharge *BohrRadius / (V_c*1000))
+e_11 = 9.5 * Coulomb / Meter**2
+h_11 = (1-1j*h_LA) * np.real(beta_11 * e_11)
+h_tilde = e_11 / ((N_c * ElectronCharge * BohrRadius)/V_c)
 
 xi_11_over_S = h_tilde * 4 * np.pi * ElectronCharge * N_S / V_c * M_S
 xi_11 =  xi_11_over_S * S_schiff # piezoaxionic tensor for nuclear coupling
